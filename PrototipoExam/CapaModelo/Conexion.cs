9aba20f2563c9e25a1892b32e4b769386abde0ba
JonathanArriaga10 <jonathanivan.a.c@gmail.com>
2023-11-21 09:48:47 -0600

    Main

---
+++ PrototipoExam/CapaModelo/Conexion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+    class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+            //creacion de la conexion via ODBC
+            //DSN CAMBIA CON ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=HotelSConexion");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+
+    }
+}
---
+++ PrototipoExam/CapaVista/FormularioPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class FormularioPrincipal : Form
+    {
+        Controlador cn = new Controlador();
+        string tabla = "tbl_estados";
+        public FormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, "ayudassss/AyudaLogistica.chm", "MDI_Inventario.html");
+            //Help.ShowHelp(this, "AYUDA/AyudaExamen.chm", "Manual usuario.html");
+            //Help.ShowHelp(this, "AYUDA LA SECUELA/ayudase.chm", "Laboratorio 5.html");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTbl(tabla);
+            dataGridView1.DataSource = dt;
+
+        }
+
+    }
+}
